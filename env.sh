b2df211c91ab50a300214b6391cbd813c1a89de2
J1nx <admin@j1nx.nl>
2013-03-17 18:35:09 +0000

    [f16ref] add env.sh to ignored files

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@ export ARCH=arm
 export CROSS_COMPILE=/root/buildroot-AmLogic/output/external-toolchain/bin/arm-none-linux-gnueabi-
 #export CROSS_COMPILE=arm-none-linux-gnueabi-
 make clean
-make meson_reff16_gbox_defconfig
-make menuconfig
+make meson_reff16_gbox_1_defconfig
+#make menuconfig
 #make vmlinux
 
 TOP=${PWD}
